ce7c07d8151444291504f2aac29f89fd0560fc4b
Mobin <mobinprogrammer2001@gmail.com>
2022-12-16 21:29:55 +0330

    word counter

---
+++ word_counter.py
@@ -0,0 +1,15 @@
+def word(x):
+    #تبدیل شمارش ها به دیکشنری
+    counts = dict()
+    #به هر فاصله که برسه اون رو یه کلمه حساب میکنه
+    words = x.split(' ')
+    
+    for word in words:
+        if word in counts:
+            #شرط رو چک میکنه اگه کلمه قبلا باشه یدونه به تعداد موجودیتش اضافه میکنه
+            counts[word] += 1
+        else:
+            #شرط رو چک میکنه اگه کلمه قبلا نباشه یدونه به تعداد موجودیتش اضافه میکنه
+            counts[word] = 1
+    return counts
+print(word('i want to tell you how much i love you'))
